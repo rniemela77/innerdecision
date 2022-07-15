e94140c4f68781368a5127ead1fcbb835687aa85
Robert Niemela <rvniemela@hotmail.com>
2022-07-15 16:53:26 -0700

    chore(inertia): set up client-side (vue) adapter

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,10 +6,10 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Inner Decision</title>
-    <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet"/>
     <script src="{{ mix('/js/app.js') }}" defer></script>
 </head>
 <body>
-    @inertia
+@inertia
 </body>
 </html>
